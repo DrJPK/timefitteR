85653678dbb6d12d7aa701d34ba6ae590c1c1c08
DrJPK <john.kennedy@unisa.edu.au>
2025-12-15 15:46:19 +1030

    convert coerce_x_numeric to a more generic coerce_var_numeric()

--- R/helpers-coerce_var_numeric.R
+++ R/helpers-coerce_var_numeric.R
@@ -1,7 +1,7 @@
-#' coerce_x_numeric
+#' coerce_var_numeric
 #'
 #' @description
-#' This function is used **internally** to coerce a column of a dataframe into a numeric format suitable for time fitting.
+#' This function is used **internally** to coerce a given column of a dataframe into a numeric format suitable for time fitting.
 #'
 #' @details
 #' While time data should generally be organised into order by hand as part of the data cleaning and preparation steps, it is sometimes necessary to do this on the fly.This function is able to take a column vector `x` usually from a dataframe in a number of formats and manipulate this in a predictable way to return an equivalent numeric vector.
@@ -11,19 +11,18 @@
 #' @param suffix An optional string that can be removed from the end of the values in x to get a number
 #'
 #' @returns A vector of the same length as x coerced to numeric type or an error.
-#' @export
 #'
 #' @examples
 #' x <- as.factor(letters[seq_len(10)])
-#' timefitteR_coerce_x_numeric(x)
+#' timefitteR_coerce_var_numeric(x)
 #'
 #' x <- as.character(paste0("Time",sprintf("%02d", sample(1:10, 50, replace = TRUE))))
-#' timefitteR_coerce_x_numeric(x, prefix = "Time")
+#' timefitteR_coerce_var_numeric(x, prefix = "Time")
 #'
 
-timefitteR_coerce_x_numeric <- function(x,
-                                        prefix = NULL,
-                                        suffix = NULL) {
+.coerce_var_numeric <- function(x,
+                                      prefix = NULL,
+                                      suffix = NULL) {
 
   col_name <- rlang::as_label(rlang::enexpr(x))
 
@@ -38,7 +37,7 @@ timefitteR_coerce_x_numeric <- function(x,
       glue::glue(
         "Column `{col_name}` is a factor; converting to numeric codes based on factor levels."
       ),
-      class = "timefitteR_x_coerced_factor"
+      class = "timefitteR_var_coerced_factor"
     )
     return(as.numeric(x))
   }
@@ -79,7 +78,7 @@ timefitteR_coerce_x_numeric <- function(x,
           "Column `{col_name}` could not be coerced to numeric even after ",
           "trimming prefix/suffix. I give up."
         ),
-        class = "timefitteR_x_not_numeric",
+        class = "timefitteR_var_not_numeric",
         call  = NULL
       )
     }
@@ -89,7 +88,7 @@ timefitteR_coerce_x_numeric <- function(x,
         "Column `{col_name}` was coerced from character to numeric ",
         "after trimming prefix/suffix."
       ),
-      class = "timefitteR_x_coerced_character"
+      class = "timefitteR_var_coerced_character"
     )
 
     return(num)
@@ -100,7 +99,7 @@ timefitteR_coerce_x_numeric <- function(x,
     message = glue::glue(
       "Column `{col_name}` must be numeric, factor, or character; got `{class(x)[1L]}`."
     ),
-    class = "timefitteR_x_not_numeric",
+    class = "timefitteR_var_not_numeric",
     call  = NULL
   )
 }
